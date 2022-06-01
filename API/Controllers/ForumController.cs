f0852ec8ff56f68e6d9d026b58b77e5c50e89a95
Dumi23 <malidamjan@outlook.com>
2022-06-01 17:44:00 +0200

    Forum Controller Fix 2.0

--- API/Controllers/ForumController.cs
+++ API/Controllers/ForumController.cs
@@ -28,7 +28,7 @@ namespace API.Controllers
         [HttpGet]
         public async Task<ActionResult<List<Post>>> GetPosts()
         {
-            return await    (new PostList.Query());
+            return await Mediator.Send(new PostList.Query());
         }
 
         [HttpGet("{id}")]
@@ -48,5 +48,46 @@ namespace API.Controllers
         {
             return await Mediator.Send(new PostReplyId.Query{Id = id});
         }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateForum(Forum forum)
+        {
+            return Ok(await Mediator.Send(new ForumCreate.Command {Forum = forum}));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreatePost(Post post)
+        {
+            return Ok(await Mediator.Send(new PostCreate.Command {Post = post}));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreatePostReply(PostReply postReply)
+        {
+            return Ok(await Mediator.Send(new PostReplyCreate.Command{Post = post}));
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> EditForum(int id, Forum forum)
+        {
+            forum.Id = id;
+            return Ok(await Mediator.Send(new ForumEdit.Command{Forum = forum}));
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> EditPost(int id, Post post)
+        {
+            post.Id = id;
+            return Ok(await Mediator.Send(new PostEdit.Command{Post = post}));
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> EditPostReply(int id, PostReply postReply)
+        {
+            postReply.Id = id;
+            return Ok(await Mediator.Send(new PostReplyEdit.Command{PostReply = postReply}));
+        }
+
+        
     }
-}
+}   
